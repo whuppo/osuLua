0d18ea753eeca2cd448bf93749d4d9b8454546fc
Jonjon Decasa <whuppo@whuppo.com>
2015-07-17 00:31:26 -0700

    expanded argument names
    and also fixed a stupid mistake when you copypasta stuff

--- osuapi.lua
+++ osuapi.lua
@@ -16,18 +16,18 @@ osu.APIGetUserBest   = osu.APIUrl .. "get_user_best"
 osu.APIGetUserRecent = osu.APIUrl .. "get_user_recent"
 osu.APIGetMatch      = osu.APIUrl .. "get_match"
 
-function osu:GetBeatmaps( k, since, s, b, u, type, m, a, limit )
-	if not k then error( "API key expected.", 2 ) end
+function osu:GetBeatmaps( key, since, beatmapset_id, beatmap_id, user_id, user_type, mode, autoconvert, limit )
+	if not key then error( "API key expected.", 2 ) end
 
-	local fetch = "?k=" .. k
-	if since then fetch = fetch .. "&since=" .. since end
-	if s     then fetch = fetch .. "&s="     .. s end
-	if b     then fetch = fetch .. "&b="     .. b end
-	if u     then fetch = fetch .. "&u="     .. u end
-	if type  then fetch = fetch .. "&type="  .. type end
-	if m     then fetch = fetch .. "&m="     .. m end
-	if a     then fetch = fetch .. "&a="     .. a end
-	if limit then fetch = fetch .. "&limit=" .. limit end
+	local fetch = "?k=" .. key
+	if since         then fetch = fetch .. "&since=" .. since end
+	if beatmapset_id then fetch = fetch .. "&s="     .. beatmapset_id end
+	if beatmap_id    then fetch = fetch .. "&b="     .. beatmap_id end
+	if user_id       then fetch = fetch .. "&u="     .. user_id end
+	if user_type     then fetch = fetch .. "&type="  .. user_type end
+	if mode          then fetch = fetch .. "&m="     .. mode end
+	if autoconvert   then fetch = fetch .. "&a="     .. autoconvert end
+	if limit         then fetch = fetch .. "&limit=" .. limit end
 
 	local data = HTTP.request( osu.APIGetBeatmaps .. fetch )
 	data = JSON:decode( data )
@@ -37,13 +37,13 @@ function osu:GetBeatmaps( k, since, s, b, u, type, m, a, limit )
 	return data
 end
 
-function osu:GetUser( k, u, m, type, event_days )
-	if not k then error( "API key expected.", 2 ) end
-	if not u then error( "user_id or a username expected.", 2 ) end
+function osu:GetUser( key, user_id, mode, user_type, event_days )
+	if not key then error( "API key expected.", 2 ) end
+	if not user_id then error( "user_id or a username expected.", 2 ) end
 
-	local fetch = "?k=" .. k .. "&u=" .. u
-	if m          then fetch = fetch .. "&m="          .. m end
-	if type       then fetch = fetch .. "&type="       .. type end
+	local fetch = "?k=" .. key .. "&u=" .. user_id
+	if mode       then fetch = fetch .. "&m="          .. mode end
+	if user_type  then fetch = fetch .. "&type="       .. user_type end
 	if event_days then fetch = fetch .. "&event_days=" .. event_days end
 
 	local data = HTTP.request( osu.APIGetUser .. fetch )
@@ -54,14 +54,14 @@ function osu:GetUser( k, u, m, type, event_days )
 	return data[ 1 ]
 end
 
-function osu:GetScores( k, b, u, m, type )
-	if not k then error( "API key expected.", 2 ) end
-	if not b then error( "beatmap_id expected.", 2 ) end
+function osu:GetScores( key, beatmap_id, user_id, mode, user_type )
+	if not key then error( "API key expected.", 2 ) end
+	if not beatmap_id then error( "beatmap_id expected.", 2 ) end
 
-	local fetch = "?k=" .. k .. "&b=" .. b
-	if u    then fetch = fetch .. "&u="    .. u end
-	if m    then fetch = fetch .. "&m="    .. m end
-	if type then fetch = fetch .. "&type=" .. type end
+	local fetch = "?k=" .. key .. "&b=" .. beatmap_id
+	if user_id   then fetch = fetch .. "&u="    .. user_id end
+	if mode      then fetch = fetch .. "&m="    .. mode end
+	if user_type then fetch = fetch .. "&type=" .. user_type end
 
 	local data = HTTP.request( osu.APIGetScores .. fetch )
 	data = JSON:decode( data )
@@ -71,31 +71,14 @@ function osu:GetScores( k, b, u, m, type )
 	return data
 end
 
-function osu:GetUserBest( k, u, m, limit, type )
-	if not k then error( "API key expected.", 2 ) end
-	if not u then error( "user_id or a username expected.", 2 ) end
+function osu:GetUserBest( key, user_id, mode, limit, user_type )
+	if not key then error( "API key expected.", 2 ) end
+	if not user_id then error( "user_id or a username expected.", 2 ) end
 
-	local fetch = "?k=" .. k .. "&u=" .. u
-	if m     then fetch = fetch .. "&m="     .. m end
-	if limit then fetch = fetch .. "&limit=" .. limit end
-	if type  then fetch = fetch .. "&type="  .. type end
-
-	local data = HTTP.request( osu.APIGetUserBest .. fetch )
-	data = JSON:decode( data )
-
-	if data.error then error( data.error, 2 ) end
-
-	return data
-end
-
-function osu:GetUserRecent( k, u, m, limit, type )
-	if not k then error( "API key expected.", 2 ) end
-	if not u then error( "user_id or a username expected.", 2 ) end
-
-	local fetch = "?k=" .. k .. "&u=" .. u
-	if m     then fetch = fetch .. "&m="     .. m end
-	if limit then fetch = fetch .. "&limit=" .. limit end
-	if type  then fetch = fetch .. "&type="  .. type end
+	local fetch = "?k=" .. key .. "&u=" .. user_id
+	if mode      then fetch = fetch .. "&m="     .. mode end
+	if limit     then fetch = fetch .. "&limit=" .. limit end
+	if user_type then fetch = fetch .. "&type="  .. user_type end
 
 	local data = HTTP.request( osu.APIGetUserBest .. fetch )
 	data = JSON:decode( data )
@@ -105,13 +88,30 @@ function osu:GetUserRecent( k, u, m, limit, type )
 	return data
 end
 
-function osu:GetMatch( k, mp )
-	if not k then error( "API key expected.", 2 ) end
-	if not mp then error( "match id expected.", 2 ) end
+function osu:GetUserRecent( key, user_id, mode, limit, user_type )
+	if not key then error( "API key expected.", 2 ) end
+	if not user_id then error( "user_id or a username expected.", 2 ) end
 
-	local fetch = "?k=" .. k .. "&mp=" .. mp
+	local fetch = "?k=" .. key .. "&u=" .. user_id
+	if mode      then fetch = fetch .. "&m="     .. mode end
+	if limit     then fetch = fetch .. "&limit=" .. limit end
+	if user_type then fetch = fetch .. "&type="  .. user_type end
 
-	local data = HTTP.request( osu.APIGetUserBest .. fetch )
+	local data = HTTP.request( osu.APIGetUserRecent .. fetch )
+	data = JSON:decode( data )
+
+	if data.error then error( data.error, 2 ) end
+
+	return data
+end
+
+function osu:GetMatch( key, match_id )
+	if not key then error( "API key expected.", 2 ) end
+	if not match_id then error( "match id expected.", 2 ) end
+
+	local fetch = "?k=" .. key .. "&mp=" .. match_id
+
+	local data = HTTP.request( osu.APIGetMatch .. fetch )
 	data = JSON:decode( data )
 
 	if data.error then error( data.error, 2 ) end
