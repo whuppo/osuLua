1d9f51fb9124653df6f09d1c93180fbe7f874771
Jonjon Decasa <whuppo@whuppo.com>
2015-07-16 23:12:41 -0700

    forgot to close my if statements..

--- osuapi.lua
+++ osuapi.lua
@@ -18,14 +18,14 @@ function osu.GetBeatmaps( k, since, s, b, u, type, m, a, limit )
 	if not k then error( "API key expected.", 2 ) end
 
 	local fetch = "?k=" .. k
-	if since then fetch = fetch .. "&since=" .. since
-	if s     then fetch = fetch .. "&s="     .. s
-	if b     then fetch = fetch .. "&b="     .. b
-	if u     then fetch = fetch .. "&u="     .. u
-	if type  then fetch = fetch .. "&type="  .. type
-	if m     then fetch = fetch .. "&m="     .. m
-	if a     then fetch = fetch .. "&a="     .. a
-	if limit then fetch = fetch .. "&limit=" .. limit
+	if since then fetch = fetch .. "&since=" .. since end
+	if s     then fetch = fetch .. "&s="     .. s end
+	if b     then fetch = fetch .. "&b="     .. b end
+	if u     then fetch = fetch .. "&u="     .. u end
+	if type  then fetch = fetch .. "&type="  .. type end
+	if m     then fetch = fetch .. "&m="     .. m end
+	if a     then fetch = fetch .. "&a="     .. a end
+	if limit then fetch = fetch .. "&limit=" .. limit end
 
 	local data = HTTP.request( osu.APIGetBeatmaps .. fetch )
 	data = JSON:decode( data )
@@ -40,9 +40,9 @@ function osu.GetUser( k, u, m, type, event_days )
 	if not u then error( "user_id or a username expected.", 2 ) end
 
 	local fetch = "?k=" .. k .. "&u=" .. u
-	if m          then fetch = fetch .. "&m="          .. m
-	if type       then fetch = fetch .. "&type="       .. type
-	if event_days then fetch = fetch .. "&event_days=" .. event_days
+	if m          then fetch = fetch .. "&m="          .. m end
+	if type       then fetch = fetch .. "&type="       .. type end
+	if event_days then fetch = fetch .. "&event_days=" .. event_days end
 
 	local data = HTTP.request( osu.APIGetUser .. fetch )
 	data = JSON:decode( data )
@@ -57,9 +57,9 @@ function osu.GetScores( k, b, u, m, type )
 	if not b then error( "beatmap_id expected.", 2 ) end
 
 	local fetch = "?k=" .. k .. "&b=" .. b
-	if u    then fetch = fetch .. "&u="    .. u
-	if m    then fetch = fetch .. "&m="    .. m
-	if type then fetch = fetch .. "&type=" .. type
+	if u    then fetch = fetch .. "&u="    .. u end
+	if m    then fetch = fetch .. "&m="    .. m end
+	if type then fetch = fetch .. "&type=" .. type end
 
 	local data = HTTP.request( osu.APIGetScores .. fetch )
 	data = JSON:decode( data )
@@ -74,9 +74,9 @@ function osu.GetUserBest( k, u, m, limit, type )
 	if not u then error( "user_id or a username expected.", 2 ) end
 
 	local fetch = "?k=" .. k .. "&u=" .. u
-	if m     then fetch = fetch .. "&m="     .. m
-	if limit then fetch = fetch .. "&limit=" .. limit
-	if type  then fetch = fetch .. "&type="  .. type
+	if m     then fetch = fetch .. "&m="     .. m end
+	if limit then fetch = fetch .. "&limit=" .. limit end
+	if type  then fetch = fetch .. "&type="  .. type end
 
 	local data = HTTP.request( osu.APIGetUserBest .. fetch )
 	data = JSON:decode( data )
@@ -91,9 +91,9 @@ function osu.GetUserRecent( k, u, m, limit, type )
 	if not u then error( "user_id or a username expected.", 2 ) end
 
 	local fetch = "?k=" .. k .. "&u=" .. u
-	if m     then fetch = fetch .. "&m="     .. m
-	if limit then fetch = fetch .. "&limit=" .. limit
-	if type  then fetch = fetch .. "&type="  .. type
+	if m     then fetch = fetch .. "&m="     .. m end
+	if limit then fetch = fetch .. "&limit=" .. limit end
+	if type  then fetch = fetch .. "&type="  .. type end
 
 	local data = HTTP.request( osu.APIGetUserBest .. fetch )
 	data = JSON:decode( data )
